f1772cf116b14409c3ea7c9054ece2d887151cb3
Devi Tse <devitcf@gmail.com>
2021-10-27 22:32:26 +0800

    Fetch data from MongoDB

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import pymongo
+
+# Initialize mongodb connection
+client = pymongo.MongoClient("mongodb+srv://{0}:{1}@cluster0.igs5e.mongodb.net/?retryWrites=true&w=majority".format(st.secrets['mongo']['username'], st.secrets['mongo']['password']))
+db = client[st.secrets['mongo']['dbname']]
 
 st.title('Login')
+# Pull data from the collection
+# Uses st.cache to only rerun when the query changes or after 10 min
+@st.cache(ttl=600)
+def get_users():
+    db = client.orcabase
+    items = db.users.find()
+    items = list(items)  # make hashable for st.cache
+    return items
+
+items = get_users()
+
+items
